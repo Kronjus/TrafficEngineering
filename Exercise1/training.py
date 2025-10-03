5718c936f0b7b684d958d861538fea6401fc3058
Julian <jujansen@student.ethz.ch>
2025-10-03 19:42:50 +0200

    Enhance training script and add configuration files for YOLO model tracking

--- Exercise1/training.py
+++ Exercise1/training.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
+# train 6 = good
 if __name__ == "__main__":
     model = YOLO("yolo11n.pt")
-    results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+    results = model.train(data="VisDrone.yaml", epochs=100, imgsz=960, patience=10, batch=8, mosaic=1.0, mixup=0.15,
+                          copy_paste=0.1)
